e2a711395766abd306bdcfdfb2b158e9b43657b8
Jemsheer K D <jemsheer.k@thinkpalm.com>
2023-01-11 15:58:34 +0530

    Code optimisation

--- lib/src/application/core/base_bloc.dart
+++ lib/src/application/core/base_bloc.dart
@@ -5,7 +5,7 @@ import 'package:thinkhub/src/application/core/base_bloc_state.dart';
 import 'package:thinkhub/src/application/core/process_state.dart';
 
 abstract class BaseBloc<Event extends BaseBlocEvent,
-    State extends BaseBlocState> extends Bloc<BaseBlocEvent, BaseBlocState> {
+    State extends BaseBlocState> extends Bloc<Event, State> {
   final processState = BehaviorSubject<ProcessState>();
 
   BaseBloc(super.initialState);
